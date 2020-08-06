e3b86cb84eb99aa4c934f622631d4289023568b7
Josh Zarrabi <jzarrabi@vmware.com>
2020-08-06 19:18:22 +0000

    rename integration file to end with _test.go
    Co-authored-by: Frankie Gallina-Jones <frankieg@vmware.com>

--- integration/without_gem_rake_task_test.go
+++ integration/without_gem_rake_task_test.go
@@ -55,7 +55,7 @@ func testWithoutGemRakeTask(t *testing.T, context spec.G, it spec.S) {
 			Expect(err).NotTo(HaveOccurred())
 
 			var logs fmt.Stringer
-			image, logs, err = pack.WithVerbose().WithNoColor().Build.
+			image, logs, err = pack.WithNoColor().Build.
 				WithBuildpacks(
 					settings.Buildpacks.MRI.Online,
 					settings.Buildpacks.Rake.Online,
