8c9c93aca6aa5c98a9f5b87b52d014db2ed54c7d
Forest Eckhardt <feckhardt@pivotal.io>
2023-05-02 16:42:16 +0000

    Adds a message to packit.Fail in detect

--- detect_test.go
+++ detect_test.go
@@ -223,7 +223,7 @@ func testDetect(t *testing.T, context spec.G, it spec.S) {
 			_, err := detect(packit.DetectContext{
 				WorkingDir: workingDir,
 			})
-			Expect(err).To(MatchError(packit.Fail))
+			Expect(err).To(MatchError(packit.Fail.WithMessage("no 'Rakefile', 'Rakefile.rb', 'rakefile', or 'rakefile.rb' file found")))
 		})
 	})
 
